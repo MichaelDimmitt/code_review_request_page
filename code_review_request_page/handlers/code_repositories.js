9bad1d7e3bd7a0493d0e0dad2223ff4f49b4b072
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-06-14 07:55:38 -0400

    quick and dirty query that gives a list of repos.

--- code_review_request_page/handlers/code_repositories.js
+++ code_review_request_page/handlers/code_repositories.js
@@ -1,14 +1,15 @@
 'use strict';
 var dataProvider = require('../data/code_repositories.js');
+const axios = require('axios')
 /**
  * Operations on /code_repositories
  */
 module.exports = {
     /**
      * summary: list repositories related to specific information
-     * description: 
+     * description:
      * parameters: limit
-     * produces: 
+     * produces:
      * responses: 200, default
      */
     get: function listRepositories(req, res, next) {
@@ -16,21 +17,24 @@ module.exports = {
          * Get the data for response 200
          * For response `default` status 200 is used.
          */
-        var status = 200;
-        var provider = dataProvider['get']['200'];
-        provider(req, res, function (err, data) {
-            if (err) {
-                next(err);
-                return;
-            }
-            res.status(status).send(data && data.responses);
+        let username=req.query.username
+        console.log(username)
+        axios.get('https://api.github.com/users/'+username+'/repos')
+        .then(function(response){
+          let arr = [];
+          let counter = 0;
+          for(counter = 0; counter < response.data.length; counter++){
+            arr.push(response.data[counter].name)
+          }
+          console.log(response.status); // ex.: 200
+          res.json(arr); // ex.: { user: 'Your User'}
         });
     },
     /**
      * summary: Write a repo to the mongo query
-     * description: 
-     * parameters: 
-     * produces: 
+     * description:
+     * parameters:
+     * produces:
      * responses: 201, default
      */
     post: function createRepositories(req, res, next) {
